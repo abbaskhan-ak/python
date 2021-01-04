f84a71f4e6205a6b176f46377247b8095b6a5b27
abbaskhan-ak <75530718+abbaskhan-ak@users.noreply.github.com>
2021-01-04 19:25:01 +0500

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+import welcome         #import ka matlab hm library ko call karrha hay
+welcome.greeting("abbas")   #ismay welcome.greeting ka matlab welcome library may jo function greeting ha
+
+import welcome as wel   #as say nam change hojai ga
+wel.greeting("ak")
+
+from welcome import greeting  #ismay . nhi lagai gay direct library call hogai
+greeting("khan")
